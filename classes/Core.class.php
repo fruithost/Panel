07b4a9e577577e35427c3e639a162119607b7305
Adrian Preuß <adrian.preuss@icloud.com>
2019-05-12 21:30:57 +0200

    Adding usable admin overview

--- classes/Core.class.php
+++ classes/Core.class.php
@@ -222,6 +222,74 @@
 				}
 				
 				switch($destination) {
+					case 'server':
+						$uname			= explode(' ', shell_exec('uname -a'));
+
+						$uptime_array	= explode(" ", exec("cat /proc/uptime"));
+						$seconds		= round($uptime_array[0], 0);
+						$minutes		= $seconds / 60;
+						$hours			= $minutes / 60;
+						$days			= floor($hours / 24);
+						$hours			= sprintf('%02d', floor($hours - ($days * 24)));
+						$minutes		= sprintf('%02d', floor($minutes - ($days * 24 * 60) - ($hours * 60)));
+						
+						if ($days == 0) {
+							$uptime = $hours . ":" .  $minutes . " (hh:mm)";
+						} elseif($days == 1) {
+							$uptime = $days . " day, " .  $hours . ":" .  $minutes . " (hh:mm)";
+						} else {
+							$uptime = $days . " days, " .  $hours . ":" .  $minutes . " (hh:mm)";
+						}
+
+						$memory		= [];
+						$meminfo	= file('/proc/meminfo', \FILE_SKIP_EMPTY_LINES);
+						
+						foreach($meminfo AS $line) {
+							preg_match('/(?<name>[a-zA-Z]+):([\s]+)(?<value>[0-9]+)\s(?<size>[a-zA-Z]+)$/Uis', $line, $matches);
+							
+							if(!empty($matches['name'])) {
+								switch(trim($matches['name'])) {
+									case 'MemTotal':	$memory['total'] = $matches['value'];		break;
+									case 'MemFree':		$memory['free'] = $matches['value'];		break;
+									case 'SwapTotal':	$memory['total_swap'] = $matches['value'];	break;
+									case 'SwapFree':	$memory['free_swap'] = $matches['value'];	break;
+									case 'Buffers':		$memory['buffer'] = $matches['value'];		break;
+									case 'Cached':		$memory['cache'] = $matches['value'];		break;
+									default: break;
+								}
+							}
+						}
+
+						$disks	= [];
+						$output	= shell_exec('df -T -h');
+						
+						foreach(explode(PHP_EOL, $output) AS $index => $line) {
+							if($index === 0 || empty(trim($line))) {
+								continue;
+							}
+							
+							preg_match('/^(?P<filesystem>[\da-zA-Z\/]+)\s+(?P<type>[\da-zA-Z\/]+)\s+(?P<size>[0-9A-Z\.]+)\s+(?P<used>[0-9A-Z\.]+)\s+(?P<avail>[0-9A-Z\.]+)\s+(?P<percent>\d+%)\s+(?P<mount>[\da-zA-Z\/]+)$/', $line, $matches);
+							
+							$disks[] = [
+								'filesystem'	=> (isset($matches['filesystem']) ? $matches['filesystem'] : NULL),
+								'type'			=> (isset($matches['type']) ? $matches['type'] : NULL),
+								'size'			=> (isset($matches['size']) ? $matches['size'] : NULL),
+								'used'			=> (isset($matches['used']) ? $matches['used'] : NULL),
+								'avail'			=> (isset($matches['avail']) ? $matches['avail'] : NULL),
+								'percent'		=> (isset($matches['percent']) ? $matches['percent'] : NULL),
+								'mount'			=> (isset($matches['mount']) ? $matches['mount'] : NULL)
+							];
+						}
+						
+						$data['hostname']		= exec('hostname -f');
+						$data['time_system']	= exec('date +\'%d %b %Y %T %Z\'');
+						$data['time_php']		= date('d M Y H:i:s T');
+						$data['os']				= $uname[0];
+						$data['kernel']			= $uname[2];
+						$data['uptime']			= $uptime;
+						$data['memory']			= $memory;
+						$data['disks']			= $disks;
+					break;
 					case 'logs':
 						$logfiles		= [];
 						$position		= 0;
--- default/admin/server.php
+++ default/admin/server.php
@@ -1,5 +1,6 @@
 <?php
 	use fruithost\Auth;
+	use Gauge\Gauge;
 	
 	$template->header();
 	
@@ -14,5 +15,74 @@
 		exit();
 	}
 	
+	?>
+	<div class="container mt-5 mb-5">
+		<div class="row">
+			<div class="col">
+				<img src="<?php print (new Gauge())->render('Memory', $memory['free'], 0, $memory['total'])->base64(); ?>" />
+			</div>
+			<div class="col">
+				<img src="<?php print (new Gauge())->render('CPU Load', $memory['free'], 0, $memory['total'])->base64(); ?>" />
+			</div>
+			<div class="col">
+				<img src="<?php print (new Gauge())->render('Swap', $memory['free_swap'], 0, $memory['total_swap'])->base64(); ?>" />
+			</div>
+			<div class="col">
+				<img src="<?php print (new Gauge())->render('Cache', $memory['cache'], $memory['free'], $memory['total'])->base64(); ?>" />
+			</div>
+			<div class="col">
+				<img src="<?php print (new Gauge())->render('Buffer', $memory['buffer'], 0, $memory['total'])->base64(); ?>" />
+			</div>
+		</div>
+	</div>
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col-6">
+				<h4>System Properties</h4>
+				<table class="table">
+					<tr>
+						<th>Hostname</th>
+						<td><?php print $hostname; ?></td>
+					</tr>
+					<tr>
+						<th>Time</th>
+						<td><?php print $time_system; ?></td>
+					</tr>
+					<tr>
+						<th>System</th>
+						<td><?php print $os; ?></td>
+					</tr>
+					<tr>
+						<th>Kernel</th>
+						<td><?php print $kernel; ?></td>
+					</tr>
+					<tr>
+						<th>Uptime</th>
+						<td><?php print $uptime; ?></td>
+					</tr>
+				</table>
+			</div>
+			<div class="col-6">
+				<h4>Mounted Drives</h4>
+				<?php
+					foreach($disks AS $disk) {
+						?>
+						<div class="mb-2">
+							<div class="d-flex">
+								<i class="material-icons mr-1">storage</i>
+								<strong><?php print $disk['mount']; ?></strong>
+							</div>
+							<div class="bg-secondary" style="height: 15px;" data-percentage="<?php print $disk['percent']; ?>">
+								<div class="bg-success" style="height: 100%; width: <?php print $disk['percent']; ?>"></div>
+							</div>
+							<small class="text-muted">Type: <?php print $disk['type']; ?> | FileSystem: <?php print $disk['filesystem']; ?></small>
+						</div>
+						<?php
+					}
+				?>
+			</div>
+		</div>
+	</div>
+	<?php
 	$template->footer();
 ?>
