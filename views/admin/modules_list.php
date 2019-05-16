4e74902b9b9c1aff0499f71be7577d13f9e39173
Adrian Preuß <adrian.preuss@icloud.com>
2019-05-16 23:45:16 +0200

    Fix tables

--- views/admin/modules_list.php
+++ views/admin/modules_list.php
@@ -1,58 +1,62 @@
-<table class="table table-striped table-hover">
-	<tr>
-		<th colspan="3">Module</th>
-		<th>Description</th>
-	</tr>
-	<?php
-		foreach($modules->getList() AS $module) {
-			$info = $module->getInfo();
-			?>
-			<tr class="<?php print (isset($upgradeable->{$module->getDirectory()}) ? 'table-warning' : ''); ?>">
-				<td scope="row" width="1px"><input type="checkbox" name="module[]" value="<?php print $info->getName(); ?>" /></td>
-				<td>
-					<span class="d-block badge badge-pill module-badge badge-<?php print ($module->isEnabled() ? 'success' : 'danger');?>" data-toggle="tooltip" title="<?php print ($module->isEnabled() ? 'Module is enabled.' : 'Module is disabled.');?>"></span>
-				</td>
-				<td>
-					<strong><?php print $info->getName(); ?></strong>
-					<p class="module-actions"><?php
-						$links = [];
-						
-						if($module->isEnabled()) {
-							$links['disable'] = '<a href="#" class="text-warning">Disable</a>';
-						} else {
-							$links['enable'] = '<a href="#" class="text-success">Enable</a>';
-						}
-						
-						if($module->hasSettingsPath()) {
-							$links['settings'] = sprintf('<a href="%s" class="text-primary">Settings</a>', $this->url('/admin/modules/?settings=' . $module->getDirectory()));
-						}
-						
-						if(isset($upgradeable->{$module->getDirectory()})) {
-							$links['upgrade'] = '<a href="#" class="text-warning font-weight-bold">Upgrade</a>';
-						}
-						
-						$links['deinstall'] = '<a href="#" class="text-danger">Deinstall</a>';
-						
-						print implode(' | ', $links);
-					?></p>
-				</td>
-				<td>
-					<?php
-						if(isset($upgradeable->{$module->getDirectory()})) {
-							$upgrade = $upgradeable->{$module->getDirectory()};
-							?>
-								<div class="alert alert-warning d-flex p-2" role="alert">
-									<i class="material-icons text-danger p-0">autorenew</i>
-									<p class="p-0 m-0">The module <strong><?php print $info->getName(); ?></strong> has an new upgrade to <strong>Version <?php print $upgrade->version; ?></strong>!</p>
-								</div>
-							<?php
-						}
-					?>
-					<p><?php print $info->getDescription(); ?></p>
-					<small>Version <?php print $info->getVersion(); ?> | by <a href="<?php print $info->getAuthor()->getWebsite(); ?>" target="_blank"><?php print $info->getAuthor()->getName(); ?></a></small>
-				</td>
-			</tr>
-			<?php
-		}
-	?>
+<table class="table table-borderless table-striped table-hover">
+	<thead>
+		<tr>
+			<th scope="col" colspan="3">Module</th>
+			<th scope="col">Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			foreach($modules->getList() AS $module) {
+				$info = $module->getInfo();
+				?>
+				<tr class="<?php print (isset($upgradeable->{$module->getDirectory()}) ? 'table-warning' : ''); ?>">
+					<td scope="row" width="1px"><input type="checkbox" name="module[]" value="<?php print $info->getName(); ?>" /></td>
+					<td>
+						<span class="d-block badge badge-pill module-badge badge-<?php print ($module->isEnabled() ? 'success' : 'danger');?>" data-toggle="tooltip" title="<?php print ($module->isEnabled() ? 'Module is enabled.' : 'Module is disabled.');?>"></span>
+					</td>
+					<td>
+						<strong><?php print $info->getName(); ?></strong>
+						<p class="module-actions"><?php
+							$links = [];
+							
+							if($module->isEnabled()) {
+								$links['disable'] = '<a href="#" class="text-warning">Disable</a>';
+							} else {
+								$links['enable'] = '<a href="#" class="text-success">Enable</a>';
+							}
+							
+							if($module->hasSettingsPath()) {
+								$links['settings'] = sprintf('<a href="%s" class="text-primary">Settings</a>', $this->url('/admin/modules/?settings=' . $module->getDirectory()));
+							}
+							
+							if(isset($upgradeable->{$module->getDirectory()})) {
+								$links['upgrade'] = '<a href="#" class="text-warning font-weight-bold">Upgrade</a>';
+							}
+							
+							$links['deinstall'] = '<a href="#" class="text-danger">Deinstall</a>';
+							
+							print implode(' | ', $links);
+						?></p>
+					</td>
+					<td>
+						<?php
+							if(isset($upgradeable->{$module->getDirectory()})) {
+								$upgrade = $upgradeable->{$module->getDirectory()};
+								?>
+									<div class="alert alert-warning d-flex p-2" role="alert">
+										<i class="material-icons text-danger p-0">autorenew</i>
+										<p class="p-0 m-0">The module <strong><?php print $info->getName(); ?></strong> has an new upgrade to <strong>Version <?php print $upgrade->version; ?></strong>!</p>
+									</div>
+								<?php
+							}
+						?>
+						<p><?php print $info->getDescription(); ?></p>
+						<small>Version <?php print $info->getVersion(); ?> | by <a href="<?php print $info->getAuthor()->getWebsite(); ?>" target="_blank"><?php print $info->getAuthor()->getName(); ?></a></small>
+					</td>
+				</tr>
+				<?php
+			}
+		?>
+	</tbody>
 </table>
--- views/admin/repository_list.php
+++ views/admin/repository_list.php
@@ -1,35 +1,39 @@
 <?php
 	use fruithost\Auth;
 ?>
-<table class="table table-striped table-hover">
-	<tr>
-		<th colspan="2">Repository</th>
-		<th>Status</th>
-		<th>Actions</th>
-	</tr>
-	<?php
-		foreach($repositorys AS $repository) {
-			?>
-			<tr>
-				<td scope="row" width="1px"><input type="checkbox" name="repository[]" value="<?php print $repository->id; ?>" /></td>
-				<td>
-					<a href="<?php print $repository->url; ?>" target="_blank"><?php print $repository->url; ?></a>
-				</td>
-				<td>
-					<?php
-						if(empty($repository->time_updated)) {
-							print '<span class="text-warning">Never updated</span>';
-						} else {
-							print date(Auth::getSettings('TIME_FORMAT', NULL, 'd.m.Y - H:i'), strtotime($repository->time_updated));
-						}
-					?>
-				</td>
-				<td>
-					<button class="update btn btn-sm btn-info" type="submit" name="action" value="update" id="update_<?php print $repository->id; ?>" value="<?php print $repository->id; ?>">Update</button>
-					<button class="delete btn btn-sm btn-danger" type="submit" name="action" value="delete" id="delete_<?php print $repository->id; ?>" value="<?php print $repository->id; ?>">Delete</button>
-				</td>
-			</tr>
-			<?php
-		}
-	?>
+<table class="table table-borderless table-striped table-hover">
+	<thead>
+		<tr>
+			<th scope="col" colspan="2">Repository</th>
+			<th scope="col">Status</th>
+			<th scope="col">Actions</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			foreach($repositorys AS $repository) {
+				?>
+				<tr>
+					<td scope="row" width="1px"><input type="checkbox" name="repository[]" value="<?php print $repository->id; ?>" /></td>
+					<td>
+						<a href="<?php print $repository->url; ?>" target="_blank"><?php print $repository->url; ?></a>
+					</td>
+					<td>
+						<?php
+							if(empty($repository->time_updated)) {
+								print '<span class="text-warning">Never updated</span>';
+							} else {
+								printf('<small>Last updated:</small><br />%s', date(Auth::getSettings('TIME_FORMAT', NULL, 'd.m.Y - H:i'), strtotime($repository->time_updated)));
+							}
+						?>
+					</td>
+					<td class="text-right">
+						<button class="update btn btn-sm btn-info" type="submit" name="action" value="update" id="update_<?php print $repository->id; ?>" value="<?php print $repository->id; ?>">Update</button>
+						<button class="delete btn btn-sm btn-danger" type="submit" name="action" value="delete" id="delete_<?php print $repository->id; ?>" value="<?php print $repository->id; ?>">Delete</button>
+					</td>
+				</tr>
+				<?php
+			}
+		?>
+	</tbody>
 </table>
