ab9715a113ad432b6d50a4ffc0212e944eca6571
a.preuss <preuss@iad1-shared-d12-06.dreamhost.com>
2024-08-06 05:57:05 -0700

    Fix layout & checkbox

--- default/settings.php
+++ default/settings.php
@@ -5,7 +5,7 @@
 	$template->header();
 	?>
 		<form method="post" action="<?php print $this->url('/settings' . (!empty($tab) ? '/' . $tab : '')); ?>">
-			<header class="page-header d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+			<header class="page-header d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
 				<nav aria-label="breadcrumb">
 					<ol class="breadcrumb" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16'%3E%3Cpath d='M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z' fill='%236c757d'/%3E%3C/svg%3E&#34;);">
 						<li class="breadcrumb-item<?php print (empty($submodule) ? ' active" aria-current="page' : ''); ?>">
@@ -123,9 +123,9 @@
 						<div class="form-group row">
 							<div class="col-sm-2 text-right"></div>
 							<div class="col-sm-10">
-								<div class="custom-control custom-checkbox">
-									<input class="custom-control-input" type="checkbox" name="2fa_enabled" id="2fa_enabled" value="true"<?php print (Auth::getSettings('2FA_ENABLED', NULL, 'false') === 'true' ? ' CHECKED' : ''); ?> />
-									<label class="custom-control-label" for="2fa_enabled">
+								<div class="form-check">
+									<input class="form-check-input" type="checkbox" id="2fa_enabled" name="2fa_enabled" value="true"<?php print (Auth::getSettings('2FA_ENABLED', NULL, 'false') === 'true' ? ' CHECKED' : ''); ?> />
+									<label class="form-check-label" for="2fa_enabled">
 										<?php I18N::__('Enable Two-factor authentication (2FA)'); ?>
 									</label>
 								</div>
