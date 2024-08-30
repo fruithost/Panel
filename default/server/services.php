9d553d58519bd6f1ede7c1555f73ef8efbc4af68
Adrian Preuss <a.preuss@fruithost.de>
2024-08-30 22:07:49 +0200

    Fix services running under init.d instead of systemd.

--- default/server/services.php
+++ default/server/services.php
@@ -57,56 +57,58 @@
                         <td scope="col" colspan="3"><?php I18N::__('No services found.'); ?></td>
                     </tr>
 					<?php
-				}
-				foreach($services as $service) {
-					if(empty($service) || empty(trim($service->unit))) {
-						continue;
+				} else {
+					foreach($services as $service) {
+						if(empty($service) || empty(trim($service->unit))) {
+							continue;
+						}
+						?>
+						<tr>
+							<td width="1px">
+								<span class="d-block badge module-badge text-bg-<?php print ($service->sub === 'running' ? 'success' : 'danger'); ?>"
+									  data-bs-toggle="tooltip"
+									  title="<?php print ($service->sub == 'running' ? 'Service is running.' : 'Service is stopped.'); ?>"></span>
+							</td>
+							<td>
+								<?php
+									print $service->unit;
+									if(!empty($service->description)) {
+										printf('<p><small>%s</small></p>', $service->description);
+									}
+								?>
+							</td>
+							<td class="text-end">
+								<form method="post"
+									  action="<?php print $this->url('/server/services'.(!empty($tab) ? '/'.$tab : '')); ?>">
+									<div class="btn-group mr-2">
+										<?php
+											if($service->sub === 'running') {
+												?>
+												<button type="submit" name="action" value="restart"
+														class="btn btn-sm btn-outline-warning m-0"><?php Icon::show('restart'); ?></button>
+												<button type="submit" name="action" value="stop"
+														class="btn btn-sm btn-outline-danger m-0"><?php Icon::show('stop'); ?></button>
+												<?php
+											} else {
+												?>
+												<button type="submit" name="action" value="start"
+														class="btn btn-sm btn-outline-success m-0"><?php Icon::show('start'); ?></button>
+												<?php
+											}
+										?>
+									</div>
+									<button type="submit" name="action" value="status"
+											class="btn btn-sm btn-outline-info"><?php I18N::__('Info'); ?></button>
+									<input type="hidden" name="service" value="<?php print $service->unit; ?>"/>
+								</form>
+							</td>
+						</tr>
+						<?php
 					}
-					?>
-                    <tr>
-                        <td width="1px">
-                            <span class="d-block badge module-badge text-bg-<?php print ($service->sub === 'running' ? 'success' : 'danger'); ?>"
-                                  data-bs-toggle="tooltip"
-                                  title="<?php print ($service->sub == 'running' ? 'Service is running.' : 'Service is stopped.'); ?>"></span>
-                        </td>
-                        <td>
-							<?php
-								print $service->unit;
-								if(!empty($service->description)) {
-									printf('<p><small>%s</small></p>', $service->description);
-								}
-							?>
-                        </td>
-                        <td class="text-end">
-                            <form method="post"
-                                  action="<?php print $this->url('/server/services'.(!empty($tab) ? '/'.$tab : '')); ?>">
-                                <div class="btn-group mr-2">
-									<?php
-										if($service->sub === 'running') {
-											?>
-                                            <button type="submit" name="action" value="restart"
-                                                    class="btn btn-sm btn-outline-warning m-0"><?php Icon::show('restart'); ?></button>
-                                            <button type="submit" name="action" value="stop"
-                                                    class="btn btn-sm btn-outline-danger m-0"><?php Icon::show('stop'); ?></button>
-											<?php
-										} else {
-											?>
-                                            <button type="submit" name="action" value="start"
-                                                    class="btn btn-sm btn-outline-success m-0"><?php Icon::show('start'); ?></button>
-											<?php
-										}
-									?>
-                                </div>
-                                <button type="submit" name="action" value="status"
-                                        class="btn btn-sm btn-outline-info"><?php I18N::__('Info'); ?></button>
-                                <input type="hidden" name="service" value="<?php print $service->unit; ?>"/>
-                            </form>
-                        </td>
-                    </tr>
-					<?php
 				}
 			?>
             </tbody>
+		</table>
     </div>
 <?php
 	$template->footer();
--- handler/server/services.php
+++ handler/server/services.php
@@ -5,13 +5,28 @@
 	use fruithost\UI\Button;
 	use fruithost\UI\Modal;
 	
+	$type = 'systemd';
+	
 	if(!Auth::hasPermission('SERVER::SERVICES')) {
 		$services = [];
 	} else {
 		$command = 'systemctl list-units --type service --full --all --no-pager';
 		$json    = shell_exec($command.' --output json');
 		if(empty($json)) {
-			$services = [];
+			$type		= 'init.d';
+			$services	= [];
+			$data		= shell_exec('service --status-all | sed \'s/ \[ \(\-\) \]  /-/\' | sed \'s/ \[ \(\+\) \]  /+/\' | sed \'s/ \[ \(\?\) \]  /?/\'');
+			$tiles		= explode(PHP_EOL, $data);
+			foreach($tiles AS $tile) {
+				$tile = trim($tile);
+				if(empty($tile)) {
+					continue;
+				}
+				$services[] = (object) [
+					'unit'	=> ltrim($tile, '+,-,?'),
+					'sub'	=> $tile[0] == '+' ? 'running' : '?'
+				];
+			}
 		} else {
 			$services = json_decode($json, false);
 			/* @ToDo Better check, if the used systemctl version supports json output format! */
@@ -21,6 +36,7 @@
 			}
 		}
 	}
+	
 	if(isset($_POST['action'])) {
 		if(!Auth::hasPermission('SERVER::SERVICES')) {
 			$this->assign('error', I18N::get('You have no permissions for this action!'));
@@ -35,7 +51,21 @@
 					return;
 				}
 				$service = $_POST['service'];
-				shell_exec(sprintf('systemctl start %s', $service));
+				$command = '';
+				switch($type) {
+					case 'init.d':
+						$command = sprintf('service %s start', $service);
+					break;
+					case 'systemd':
+						$command = sprintf('systemctl start %s', $service);
+					break;
+				}
+				
+				if(empty($command)) {
+					$this->assign('success', I18N::get('Unsupported operating system?'));
+					return;
+				}
+				shell_exec($command);
 				$this->assign('success', sprintf(I18N::get('The Service "%s" will be started!'), $service));
 				break;
 			case 'stop':
@@ -45,7 +75,21 @@
 					return;
 				}
 				$service = $_POST['service'];
-				shell_exec(sprintf('systemctl stop %s', $service));
+				$command = '';
+				switch($type) {
+					case 'init.d':
+						$command = sprintf('service %s stop', $service);
+					break;
+					case 'systemd':
+						$command = sprintf('systemctl stop %s', $service);
+					break;
+				}
+				
+				if(empty($command)) {
+					$this->assign('success', I18N::get('Unsupported operating system?'));
+					return;
+				}
+				shell_exec($command);
 				$this->assign('success', sprintf(I18N::get('The Service "%s" will be stopped!'), $service));
 				break;
 			case 'restart':
@@ -55,12 +99,41 @@
 					return;
 				}
 				$service = $_POST['service'];
-				shell_exec(sprintf('systemctl restart %s', $service));
+				$command = '';
+				switch($type) {
+					case 'init.d':
+						$command = sprintf('service %s restart', $service);
+					break;
+					case 'systemd':
+						$command = sprintf('systemctl restart %s', $service);
+					break;
+				}
+				
+				if(empty($command)) {
+					$this->assign('success', I18N::get('Unsupported operating system?'));
+					return;
+				}
+				shell_exec($command);
 				$this->assign('success', sprintf(I18N::get('The Service "%s" will be restarted!'), $service));
 				break;
 			case 'status':
 				$service = $_POST['service'];
-				$result  = shell_exec(sprintf('systemctl status %s', $service));
+				$command = '';
+				switch($type) {
+					case 'init.d':
+						$command = sprintf('service %s status', $service);
+					break;
+					case 'systemd':
+						$command = sprintf('systemctl status %s', $service);
+					break;
+				}
+				
+				if(empty($command)) {
+					$this->assign('success', I18N::get('Unsupported operating system?'));
+					return;
+				}
+				
+				$result  = shell_exec($command);
 				$modal   = new Modal('status', I18N::get('Status'), __DIR__.'/services.modal.php');
 				$modal->addButton([
 					(new Button())->setName('ok')->setLabel(I18N::get('OK'))->addClass('btn-outline-success')->setDismissable()
